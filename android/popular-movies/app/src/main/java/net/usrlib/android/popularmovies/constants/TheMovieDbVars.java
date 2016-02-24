337e1ea010a3b2f1da50f4fa3e58c8036665cdfe
rgr-myrg <rpmyrg@gmail.com>
2016-02-23 20:53:00 -0500

    Project Set Up

---
+++ android/popular-movies/app/src/main/java/net/usrlib/android/popularmovies/constants/TheMovieDbVars.java
@@ -0,0 +1,8 @@
+package net.usrlib.android.popularmovies.constants;
+
+/**
+ * Created by rgr-myrg on 2/23/16 8:45 PM.
+ */
+public class TheMovieDbVars {
+	public static final String API_URL = "http://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=";
+}
