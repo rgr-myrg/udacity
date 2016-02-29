6431c78c25d84886fc8e0e029e66bb8a879bb695
rgr-myrg <rpmyrg@gmail.com>
2016-02-29 16:00:15 -0500

    Code refactor. Created MovieBrowser.

--- android/movies/app/src/main/java/net/usrlib/android/movies/MainActivityFragment.java
+++ android/movies/app/src/main/java/net/usrlib/android/movies/MainActivityFragment.java
@@ -1,43 +1,23 @@
 package net.usrlib.android.movies;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AbsListView;
-import android.widget.AdapterView;
 import android.widget.GridView;
 
-import net.usrlib.android.movies.adapter.GridItemAdapter;
-import net.usrlib.android.movies.movieapi.MovieApi;
+import net.usrlib.android.movies.movieapi.MovieBrowser;
+import net.usrlib.android.movies.movieapi.MovieBrowser.MovieEventDelegate;
 import net.usrlib.android.movies.movieapi.MovieItemVO;
 import net.usrlib.android.movies.movieapi.MovieVars;
 
 import java.util.ArrayList;
 
-public class MainActivityFragment extends Fragment {
+public class MainActivityFragment extends Fragment implements MovieEventDelegate {
 
-	private GridView mGridView = null;
-	private GridItemAdapter mGridItemAdapter = null;
-	private String mCurrentSortBy;
-	private boolean mIsFirstPageRequest;
-
-	private MovieApi mMovieApi = new MovieApi(
-			new MovieApi.Delegate() {
-				@Override
-				public void onFeedLoaded(ArrayList<MovieItemVO> arrayList) {
-					onMovieFeedLoaded(arrayList);
-				}
-
-				@Override
-				public void onPageLimitReached() {
-					onMovieLimitReached();
-				}
-			}
-	);
+	private MovieBrowser mMovieBrowser = null;
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle instanceState) {
@@ -46,8 +26,15 @@ public class MainActivityFragment extends Fragment {
 		// Ensure onOptionsItemSelected is triggered
 		setHasOptionsMenu(true);
 
+		if (mMovieBrowser == null) {
+			mMovieBrowser = new MovieBrowser(this);
+		}
+
 		// Init and Set Up Grid View
-		initGridView(rootView);
+		mMovieBrowser.initGridView(
+				getActivity(),
+				(GridView) rootView.findViewById(R.id.grid_view)
+		);
 
 		if (instanceState == null) {
 			// Start up with Most Popular Movies
@@ -73,6 +60,10 @@ public class MainActivityFragment extends Fragment {
 				getHighestRatedMovies();
 				break;
 
+			case R.id.action_favorites:
+				getFavoriteMovies();
+				break;
+
 			case R.id.action_settings:
 				// Not implemented Yet
 				break;
@@ -89,86 +80,33 @@ public class MainActivityFragment extends Fragment {
 		super.onSaveInstanceState(outState);
 		outState.putParcelableArrayList(
 				MovieVars.MOVIE_LIST_KEY,
-				mGridItemAdapter.getMovieItems()
+				mMovieBrowser.getMovieItems()
 		);
 	}
 
 	private void getMostPopularMovies() {
-		getActivity().setTitle(getString(R.string.title_most_popular));
-		startNewRequestSortedBy(MovieVars.MOST_POPULAR);
+		mMovieBrowser.getMostPopularMovies(getActivity(), R.string.title_most_popular);
 	}
 
 	private void getHighestRatedMovies() {
-		getActivity().setTitle(getString(R.string.title_highest_rated));
-		startNewRequestSortedBy(MovieVars.HIGHEST_RATED);
+		mMovieBrowser.getHighestRatedMovies(getActivity(), R.string.title_highest_rated);
+	}
+
+	private void getFavoriteMovies() {
+
 	}
 
 	private void getMovieListFromBundle(Bundle bundle) {
-		onMovieFeedLoaded(
-				(ArrayList<MovieItemVO>) bundle.get(MovieVars.MOVIE_LIST_KEY)
-		);
+		mMovieBrowser.getMovieListFromBundle(bundle);
 	}
 
-	private void startNewRequestSortedBy(String sortBy) {
-		mCurrentSortBy = sortBy;
-		mIsFirstPageRequest = true;
+	@Override
+	public void onMovieFeedLoaded(ArrayList<MovieItemVO> arrayList) {
 
-		mMovieApi.fetchFirstPageSortedBy(sortBy);
 	}
 
-	private void initGridView(View rootView) {
-		mGridView = (GridView) rootView.findViewById(R.id.grid_view);
-
-		mGridView.setOnItemClickListener(new AbsListView.OnItemClickListener() {
-			@Override
-			public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-				GridItemAdapter gridItemAdapter = (GridItemAdapter) parent.getAdapter();
-				MovieItemVO movieItemVO = (MovieItemVO) gridItemAdapter.getItem(position);
-
-				if (movieItemVO == null) {
-					return;
-				}
-
-				Intent intent = new Intent(getActivity(), DetailActivity.class);
-				intent.putExtra(MovieItemVO.NAME, movieItemVO);
-
-				getActivity().startActivity(intent);
-			}
-		});
-
-		mGridView.setOnScrollListener(new AbsListView.OnScrollListener() {
-			@Override
-			public void onScrollStateChanged(AbsListView view, int scrollState) {}
-
-			@Override
-			public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount, int totalItemCount) {
-				// onScroll is triggered spuriously while GridView is created.
-				// Guard clause: Do nothing when totalItemCount is zero.
-				if (totalItemCount == 0) {
-					return;
-				}
-
-				int lastItemCount = firstVisibleItem + visibleItemCount;
-
-				if (lastItemCount == totalItemCount) {
-					mMovieApi.fetchNextPageSortedBy(mCurrentSortBy);
-				}
-			}
-		});
-	}
-
-	private void onMovieFeedLoaded(ArrayList<MovieItemVO> arrayList){
-		if (mGridItemAdapter == null || mIsFirstPageRequest) {
-			mGridItemAdapter = new GridItemAdapter(getContext(), arrayList);
-			mGridView.setAdapter(mGridItemAdapter);
-			mIsFirstPageRequest = false;
-
-		} else {
-			mGridItemAdapter.updateItemsList(arrayList);
-		}
-	}
-
-	private void onMovieLimitReached() {
+	@Override
+	public void onMovieLimitReached() {
 
 	}
 
