d46e74791a4831f48db2d50ab67e99b709a470bb
rgr-myrg <rpmyrg@gmail.com>
2016-05-02 20:00:49 -0400

    Added getJokeFromIntent

--- android/build-it-bigger/jokeandroidlib/src/main/java/net/usrlib/android/jokeandroidlib/DisplayJokeActivity.java
+++ android/build-it-bigger/jokeandroidlib/src/main/java/net/usrlib/android/jokeandroidlib/DisplayJokeActivity.java
@@ -2,12 +2,27 @@ package net.usrlib.android.jokeandroidlib;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class DisplayJokeActivity extends AppCompatActivity {
 	public static final String JOKE_KEY = "jokeKey";
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_display_joke);
+
+		final TextView textView = (TextView) findViewById(R.id.joke_text);
+		textView.setText(getJokeFromIntent());
+	}
+
+	private String getJokeFromIntent() {
+		String joke = getIntent().getStringExtra(JOKE_KEY);
+
+		if (joke == null) {
+			joke = "No Joke";
+		}
+
+		return joke;
 	}
 }
