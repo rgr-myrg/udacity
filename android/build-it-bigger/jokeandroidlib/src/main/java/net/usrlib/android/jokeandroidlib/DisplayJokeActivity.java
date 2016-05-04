c90664ca4698073f16c7480aa781073be1797256
rgr-myrg <rpmyrg@gmail.com>
2016-05-03 21:32:48 -0400

    Code clean up.

--- android/build-it-bigger/jokeandroidlib/src/main/java/net/usrlib/android/jokeandroidlib/DisplayJokeActivity.java
+++ android/build-it-bigger/jokeandroidlib/src/main/java/net/usrlib/android/jokeandroidlib/DisplayJokeActivity.java
@@ -1,7 +1,10 @@
 package net.usrlib.android.jokeandroidlib;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
 import android.widget.TextView;
 
 public class DisplayJokeActivity extends AppCompatActivity {
@@ -12,13 +15,18 @@ public class DisplayJokeActivity extends AppCompatActivity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_display_joke);
 
-		// TODO: Figure out back button issue.
-		// Back button is displayed but clicking does nothing.
+		// TODO Figure out Home Button Navigation
 //		getSupportActionBar().setDisplayShowHomeEnabled(true);
 //		getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
 		final TextView textView = (TextView) findViewById(R.id.joke_text);
 		textView.setText(getJokeFromIntent());
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		Log.d("DISPLAY", "onOptionsItemSelected");
+		return super.onOptionsItemSelected(item);
 	}
 
 	private String getJokeFromIntent() {
