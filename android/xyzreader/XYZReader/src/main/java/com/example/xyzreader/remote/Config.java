79125b37aa3894cc3807e5445f110d59f0ee2ad2
rgr-myrg <rpmyrg@gmail.com>
2016-05-05 16:29:40 -0400

    Added meaningful error message. String constants.

--- android/xyzreader/XYZReader/src/main/java/com/example/xyzreader/remote/Config.java
+++ android/xyzreader/XYZReader/src/main/java/com/example/xyzreader/remote/Config.java
@@ -1,19 +1,25 @@
 package com.example.xyzreader.remote;
 
+import android.util.Log;
+
 import java.net.MalformedURLException;
 import java.net.URL;
 
 public class Config {
-    public static final URL BASE_URL;
+	public static final String NAME = Config.class.getSimpleName();
+	public static final String BASE_URL_STRING = "https://dl.dropboxusercontent.com/u/231329/xyzreader_data/data.json";
+	public static final URL BASE_URL;
 
-    static {
-        URL url = null;
-        try {
-            url = new URL("https://dl.dropboxusercontent.com/u/231329/xyzreader_data/data.json" );
-        } catch (MalformedURLException ignored) {
-            // TODO: throw a real error
-        }
+	static {
+		URL url = null;
 
-        BASE_URL = url;
-    }
+		try {
+			url = new URL(BASE_URL_STRING);
+		} catch (MalformedURLException e) {
+			Log.e(NAME, "Unable to create URL instance with " + BASE_URL_STRING);
+			e.printStackTrace();
+		}
+
+		BASE_URL = url;
+	}
 }
