1cd39bff3943477534ee79f3a6be6ae3fb7d22a0
rgr-myrg <rpmyrg@gmail.com>
2016-05-05 16:20:08 -0400

    Changed deprecated method to use sendBroadcast() instead.

--- android/xyzreader/XYZReader/src/main/java/com/example/xyzreader/data/UpdaterService.java
+++ android/xyzreader/XYZReader/src/main/java/com/example/xyzreader/data/UpdaterService.java
@@ -21,67 +21,73 @@ import org.json.JSONObject;
 import java.util.ArrayList;
 
 public class UpdaterService extends IntentService {
-    private static final String TAG = "UpdaterService";
+	private static final String TAG = UpdaterService.class.getSimpleName();
 
-    public static final String BROADCAST_ACTION_STATE_CHANGE
-            = "com.example.xyzreader.intent.action.STATE_CHANGE";
-    public static final String EXTRA_REFRESHING
-            = "com.example.xyzreader.intent.extra.REFRESHING";
+	public static final String BROADCAST_ACTION_STATE_CHANGE
+			= "com.example.xyzreader.intent.action.STATE_CHANGE";
+	public static final String EXTRA_REFRESHING
+			= "com.example.xyzreader.intent.extra.REFRESHING";
 
-    public UpdaterService() {
-        super(TAG);
-    }
+	public UpdaterService() {
+		super(TAG);
+	}
 
-    @Override
-    protected void onHandleIntent(Intent intent) {
-        Time time = new Time();
+	@Override
+	protected void onHandleIntent(Intent intent) {
+		Time time = new Time();
 
-        ConnectivityManager cm = (ConnectivityManager) getSystemService(CONNECTIVITY_SERVICE);
-        NetworkInfo ni = cm.getActiveNetworkInfo();
-        if (ni == null || !ni.isConnected()) {
-            Log.w(TAG, "Not online, not refreshing.");
-            return;
-        }
+		ConnectivityManager cm = (ConnectivityManager) getSystemService(CONNECTIVITY_SERVICE);
+		NetworkInfo ni = cm.getActiveNetworkInfo();
+		if (ni == null || !ni.isConnected()) {
+			Log.w(TAG, "Not online, not refreshing.");
+			return;
+		}
 
-        sendStickyBroadcast(
-                new Intent(BROADCAST_ACTION_STATE_CHANGE).putExtra(EXTRA_REFRESHING, true));
+		sendBroadcast(
+				new Intent(BROADCAST_ACTION_STATE_CHANGE).putExtra(EXTRA_REFRESHING, true));
 
-        // Don't even inspect the intent, we only do one thing, and that's fetch content.
-        ArrayList<ContentProviderOperation> cpo = new ArrayList<ContentProviderOperation>();
+		// Don't even inspect the intent, we only do one thing, and that's fetch content.
+		ArrayList<ContentProviderOperation> cpo = new ArrayList<ContentProviderOperation>();
 
-        Uri dirUri = ItemsContract.Items.buildDirUri();
+		Uri dirUri = ItemsContract.Items.buildDirUri();
 
-        // Delete all items
-        cpo.add(ContentProviderOperation.newDelete(dirUri).build());
+		// Delete all items
+		cpo.add(ContentProviderOperation.newDelete(dirUri).build());
 
-        try {
-            JSONArray array = RemoteEndpointUtil.fetchJsonArray();
-            if (array == null) {
-                throw new JSONException("Invalid parsed item array" );
-            }
+		try {
+			JSONArray array = RemoteEndpointUtil.fetchJsonArray();
+			if (array == null) {
+				throw new JSONException("Invalid parsed item array" );
+			}
 
-            for (int i = 0; i < array.length(); i++) {
-                ContentValues values = new ContentValues();
-                JSONObject object = array.getJSONObject(i);
-                values.put(ItemsContract.Items.SERVER_ID, object.getString("id" ));
-                values.put(ItemsContract.Items.AUTHOR, object.getString("author" ));
-                values.put(ItemsContract.Items.TITLE, object.getString("title" ));
-                values.put(ItemsContract.Items.BODY, object.getString("body" ));
-                values.put(ItemsContract.Items.THUMB_URL, object.getString("thumb" ));
-                values.put(ItemsContract.Items.PHOTO_URL, object.getString("photo" ));
-                values.put(ItemsContract.Items.ASPECT_RATIO, object.getString("aspect_ratio" ));
-                time.parse3339(object.getString("published_date"));
-                values.put(ItemsContract.Items.PUBLISHED_DATE, time.toMillis(false));
-                cpo.add(ContentProviderOperation.newInsert(dirUri).withValues(values).build());
-            }
+			for (int i = 0; i < array.length(); i++) {
+				ContentValues values = new ContentValues();
+				JSONObject object = array.getJSONObject(i);
 
-            getContentResolver().applyBatch(ItemsContract.CONTENT_AUTHORITY, cpo);
+				values.put(ItemsContract.Items.SERVER_ID, object.getString("id" ));
+				values.put(ItemsContract.Items.AUTHOR, object.getString("author" ));
+				values.put(ItemsContract.Items.TITLE, object.getString("title" ));
+				values.put(ItemsContract.Items.BODY, object.getString("body" ));
+				values.put(ItemsContract.Items.THUMB_URL, object.getString("thumb" ));
+				values.put(ItemsContract.Items.PHOTO_URL, object.getString("photo" ));
+				values.put(ItemsContract.Items.ASPECT_RATIO, object.getString("aspect_ratio" ));
 
-        } catch (JSONException | RemoteException | OperationApplicationException e) {
-            Log.e(TAG, "Error updating content.", e);
-        }
+				time.parse3339(object.getString("published_date"));
 
-        sendStickyBroadcast(
-                new Intent(BROADCAST_ACTION_STATE_CHANGE).putExtra(EXTRA_REFRESHING, false));
-    }
+				values.put(ItemsContract.Items.PUBLISHED_DATE, time.toMillis(false));
+
+				cpo.add(ContentProviderOperation.newInsert(dirUri).withValues(values).build());
+			}
+
+			getContentResolver().applyBatch(ItemsContract.CONTENT_AUTHORITY, cpo);
+
+		} catch (JSONException | RemoteException | OperationApplicationException e) {
+			Log.e(TAG, "Error updating content.", e);
+		}
+
+		sendBroadcast(
+				new Intent(BROADCAST_ACTION_STATE_CHANGE)
+						.putExtra(EXTRA_REFRESHING, false)
+		);
+	}
 }
