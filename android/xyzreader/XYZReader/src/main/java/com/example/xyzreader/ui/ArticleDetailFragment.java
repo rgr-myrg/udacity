c9a25f40665b04c8eb43e9f6fd33bcad8a37f475
rgr-myrg <rpmyrg@gmail.com>
2016-05-10 15:48:24 -0400

    Added floating action button onclick. Removed image view set on click listener.

--- android/xyzreader/XYZReader/src/main/java/com/example/xyzreader/ui/ArticleDetailFragment.java
+++ android/xyzreader/XYZReader/src/main/java/com/example/xyzreader/ui/ArticleDetailFragment.java
@@ -149,17 +149,17 @@ public class ArticleDetailFragment extends Fragment implements
 
 		mStatusBarColorDrawable = new ColorDrawable(0);
 
-		mRootView.findViewById(R.id.share_fab).setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View view) {
-				startActivity(Intent.createChooser(ShareCompat.IntentBuilder.from(getActivity())
-						.setType(INTENT_TEXT_TYPE)
-						.setText(getString(R.string.action_share_text))
-						.getIntent(), getString(R.string.action_share))
-				);
-			}
-		});
-
+//		mRootView.findViewById(R.id.share_fab).setOnClickListener(new View.OnClickListener() {
+//			@Override
+//			public void onClick(View view) {
+//				startActivity(Intent.createChooser(ShareCompat.IntentBuilder.from(getActivity())
+//						.setType(INTENT_TEXT_TYPE)
+//						.setText(getString(R.string.action_share_text))
+//						.getIntent(), getString(R.string.action_share))
+//				);
+//			}
+//		});
+//
 		bindViews();
 		updateStatusBar();
 
